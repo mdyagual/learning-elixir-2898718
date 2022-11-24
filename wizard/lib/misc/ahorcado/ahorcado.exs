90bf414a2556ee074d2ccc8b5e6fab00608f3139
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-11-24 14:16:51 -0500

    Finished ahorcado.exs

--- wizard/lib/misc/ahorcado/ahorcado.exs
+++ wizard/lib/misc/ahorcado/ahorcado.exs
@@ -10,36 +10,30 @@ defmodule Wizard.Ahorcado do
     espacios = String.duplicate("_", String.length(selPal)-2)
     pal_j = String.first(selPal) <> espacios <> String.last(selPal)
     oport = 0
-    juego(pal_j, selPal, oport, actualizarDibujo(0))
-
-
+    juego(pal_j, selPal, oport, initDibujo())
   end
 
-  def juego(palJ, selPal, oport, dib_j) do
+  def juego(palJ, selPal, oport, dib_j) when palJ != selPal and oport < 6 do
     IO.puts(dib_j)
     IO.puts(palJ)
-    if palJ == selPal do
-      juego(palJ)
+    ingL = IO.gets("Ingrese una letra: ") |> String.trim()
+    if String.contains?(selPal, ingL) do
+      pal_j = actualizarPalabra(palJ, ingL, selPal)
+      juego(pal_j, selPal, oport, dib_j)  #cafetera, cafetera, 4, dibujito
     else
-      ingL = IO.gets("Ingrese una letra: ") |> String.trim()
-      if String.contains?(selPal, ingL) do
-        pal_j = actualizarPalabra(palJ, ingL, selPal)
-        juego(pal_j, selPal, oport, dib_j)
-      else
-        oport = oport + 1
-        dib_j = actualizarDibujo(oport, dib_j)
-        if is_atom(dib_j), do: dib_j, else: juego(palJ, selPal, oport, dib_j)
+      oport = oport + 1
+      dib_j = actualizarDibujo(oport, dib_j)
+      juego(palJ, selPal, oport, dib_j)
       end
-    end
   end
 
-  def juego(palAd), do: {:guessed, palAd}
+  def juego(palJ, selPal, _ ,_) when palJ == selPal, do: {:guessed, selPal}
 
-  def juego(oport, _) when oport == 6, do: :gameover
+  def juego(_, _, oport, dib_j) when oport == 6, do:  {:gameover, dib_j}
 
   def actualizarPalabra(palJ, ingL, selPal) do
       mod = modificaciones(selPal, ingL)
-      actualizar(mod, length(mod), palJ)
+      procesar(mod, length(mod), palJ)
   end
 
   def modificaciones(selPal, ingL) do
@@ -47,19 +41,18 @@ defmodule Wizard.Ahorcado do
     Enum.filter(Enum.with_index(l_selPal), fn(t) -> elem(t, 0) == ingL end)
   end
 
-  def actualizar(l_mods, tot, palJ) when tot > 0 do
+  def procesar(l_mods, tot, palJ) when tot > 0 do
     t = Enum.at(l_mods, length(l_mods)-tot)
     act = String.codepoints(palJ)
         |> List.replace_at(elem(t,1), elem(t,0))
         |> Enum.join("")
     tot = tot - 1
-    actualizar(l_mods, tot, act)
+    procesar(l_mods, tot, act)
   end
 
-  def actualizar(_, 0, act), do: act
+  def procesar(_, 0, act), do: act
 
-  def actualizarDibujo(oport, dib_j \\ "") do
-    partes = ["O", "|", "/", "\\", "/", "\\"]
+  def initDibujo() do
     dibujo = """
       ____
      |    |
@@ -69,29 +62,21 @@ defmodule Wizard.Ahorcado do
          _|_
 
     """
-    dibujoComp = """
-      ____
-     |    |
-     O    |
-    /|\\   |
-    / \\   |
-         _|_
+    dibujo
+  end
 
-    """
-    if oport == 0 do
-      dibujo
-    else
-      if oport == 6 do
-        IO.puts(dibujoComp)
-        juego(oport, dibujoComp)
-      else
-        String.replace(dib_j, to_string(oport), Enum.at(partes, oport - 1))
-      end
+  def actualizarDibujo(oport, dib_j) when oport <= 6 do
+    partes = ["O", "|", "/", "\\", "/", "\\"]
+    String.replace(dib_j, to_string(oport), Enum.at(partes, oport - 1))
+  end
 
-
-    end
+  def actualizarDibujo(oport, dib_j) when oport >= 6 do
+    dib_j
   end
 
 end
 
-IO.inspect(Wizard.Ahorcado.init_juego)
+{status, result} = Wizard.Ahorcado.init_juego
+IO.puts("\n")
+IO.inspect(status)
+IO.puts(result)
